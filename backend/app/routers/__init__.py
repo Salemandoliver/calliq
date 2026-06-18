5ccd0d119d7202d550b5727feb718343fc7d2d89
Salemandoliver <salem@oxfordandbucks.com>
2026-06-18 21:51:53 +0100

    Update __init__.py

--- backend/app/routers/__init__.py
+++ backend/app/routers/__init__.py
@@ -1,2 +1,2 @@
 from . import (auth_router, calls_router, insights_router, admin_router,
-               reports_router, webhooks_router)  # noqa: F401
+               reports_router, webhooks_router, teams_router)  # noqa: F401
